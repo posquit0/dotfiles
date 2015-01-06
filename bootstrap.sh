f06dea847c19d25ac58e57c5bd34787d13ba3dbe
Posquit0.BJ <posquit0.bj@gmail.com>
2015-01-06 11:27:58 +0000

    add bootstrap.sh

---
+++ bootstrap.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+echo "Installing Posquit0's dotfiles"
+
+# If Git is not installed, install it (Ubuntu only, since Git comes standard
+# with recent XCode or CLT)
+if [[ ! "$(type -P git)" && "$(cat /etc/issue 2> /dev/null)" =~ Ubuntu ]]; then
+    echo "Installing Git"
+    sudo apt-get -qq install git-core
+fi
+
+# If Git isn't installed by now, something exploded. We gots to quit!
+if [[ ! "$(type -P git)" ]]; then
+    echo "ERROR: Git should be installed. It isn't. Aborting."
+    exit 1
+fi
+
+# Download or update.
+if [[ ! -d ~/.dotfiles ]]; then
+    # ~/.dotfiles doesn't exist? Clone it!
+    echo "Cloning dotfiles to ~/.dotfiles"
+    cd
+    git clone --recursive https://github.com/posquit0/dotfiles.git ~/.dotfiles
+    cd ~/.dotfiles
+else
+    # Make sure we have the latest files.
+    echo "Updating dotfiles"
+    cd ~/.dotfiles
+    git pull
+    git submodule update --init --recursive --quiet
+fi
+
+log_info "Installation complete!"
