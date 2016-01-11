c11945a3917614bb4d839cade20346567a03903a
Posquit0.BJ <posquit0.bj@gmail.com>
2016-01-11 19:36:49 +0900

    improve motd on zsh

--- zsh/.oh-my-zsh-custom/plugins/motd/motd.plugin.zsh
+++ zsh/.oh-my-zsh-custom/plugins/motd/motd.plugin.zsh
@@ -8,8 +8,12 @@
 # Maintained by Claud D. Park <posquit0.bj@gmail.com>
 # http://www.posquit0.com/
 
+      ZSH_THEME_GIT_PROMPT_PREFIX=
+      ZSH_THEME_GIT_PROMPT_SUFFIX=
 if [[ $SHLVL -eq 1 ]] ; then
-  echo
+  echo "$fg[green]"
+  cat $(dirname -- $0)/motd
+	echo "$reset_color"
   print -P "`quote`"
   echo
 fi
