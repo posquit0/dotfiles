d35aa04a6213412817b0ac7c3490d7af702cec9b
Byungjin Park <posquit0.bj@gmail.com>
2023-09-12 16:40:11 +0900

    Update macos configure script

--- .chezmoiscripts/darwin/run_onchange_after_02_configure.sh
+++ .chezmoiscripts/darwin/run_onchange_after_02_configure.sh
@@ -2,17 +2,29 @@
 
 set -eufo pipefail
 
-
 ###############################################################################
 # Finder
 ###############################################################################
 
 # Show all filename extensions
 defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+# Show hidden files in the Finder
+defaults write com.apple.finder AppleShowAllFiles -bool true
 # Show status bar
 defaults write com.apple.finder ShowStatusBar -bool true
 # Show path bar
 defaults write com.apple.finder ShowPathbar -bool true
+# Choose the size of Finder sidebar icons
+defaults write NSGlobalDomain NSTableViewDefaultSizeMode -int 1
+# Set the default view style for folders without custom setting (Nlsv: List view, clmv: Column view, icnv: Icon view, glyv: Gallery view)
+defaults write com.apple.finder FXPreferredViewStyle -string "Nlsv"
+# Keep folders on top when sorting by name
+defaults write com.apple.finder _FXSortFoldersFirst -bool true
+# Remove items in the bin after 30 days
+defaults write com.apple.finder FXRemoveOldTrashItems -bool true
+
+# Restart Finder
+killall Finder
 
 ###############################################################################
 # Desktop & Dock
@@ -22,6 +34,10 @@ defaults write com.apple.finder ShowPathbar -bool true
 defaults write com.apple.dock orientation left
 # Automatically hide and show the Dock
 defaults write com.apple.dock autohide -bool true
+# Change the Dock opening and closing animation times (defaults to 0.5)
+defaults write com.apple.dock autohide-time-modifier -float "0.4"
+# Change the Dock opening delay (defaults to 0.2)
+defaults write com.apple.dock autohide-delay -float "0.3"
 # Don’t show recent applications in Dock
 defaults write com.apple.dock show-recents -bool false
 
@@ -31,6 +47,23 @@ defaults write com.apple.dock largesize -int 32
 
 # Minimize windows into their application’s icon
 defaults write com.apple.dock minimize-to-application -bool true
+# Change the Dock minimize animation
+defaults write com.apple.dock mineffect -string suck
+
+# Restart Dock
+killall Dock
+
+###############################################################################
+# Menu Bar
+###############################################################################
+
+# Configure the time and date format for the menubar digital clock
+defaults write com.apple.menuextra.clock DateFormat -string "\"EEE d MMM HH:mm:ss\""
+# The clock indicator (which by default is the colon) will flash on and off each second
+defaults write com.apple.menuextra.clock FlashDateSeparators -bool true
+
+# Restart Menu Bar
+killall SystemUIServer
 
 ###############################################################################
 # Mac App Store                                                               #
