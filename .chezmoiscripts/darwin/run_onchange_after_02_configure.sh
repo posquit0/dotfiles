a6588c6c1dec8002e89ea6f358e7c8c2f102d45a
Byungjin Park <posquit0.bj@gmail.com>
2023-09-12 19:01:40 +0900

    Fix

--- .chezmoiscripts/darwin/run_onchange_after_02_configure.sh
+++ .chezmoiscripts/darwin/run_onchange_after_02_configure.sh
@@ -91,10 +91,10 @@ defaults write com.apple.ActivityMonitor OpenMainWindow -bool true
 # Kill affected applications                                                  #
 ###############################################################################
 
-for app in "Activity Monitor" \
+for app in \
 	"Dock" \
 	"Finder" \
 	"SystemUIServer"; do
-	killall "${app}" &>/dev/null
+	killall ${app} &>/dev/null
 done
 echo "Done. Note that some of these changes require a logout/restart to take effect."
